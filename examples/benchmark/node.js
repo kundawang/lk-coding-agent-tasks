8c6cb47ea701a9b35d24318c573293229fe0b57f
kundawang <kundawang@users.noreply.github.com>
2026-09-24 23:36:37 +0800

    LK-132 base (2026-09-24)

---
+++ examples/benchmark/node.js
@@ -0,0 +1,6 @@
+import Benchmark from 'benchmark';
+import * as uuid from 'uuid';
+
+import benchmark from './benchmark.js';
+
+benchmark(uuid, Benchmark);
---
+++ examples/typescript/buffer.test.ts
@@ -0,0 +1,7 @@
+import { v1 } from 'uuid';
+
+v1(undefined, new Uint8Array(16)) satisfies Uint8Array;
+v1(undefined, Buffer.alloc(16)) satisfies Buffer;
+
+// @ts-expect-error
+v1(undefined, new Uint8Array(16)) satisfies Buffer;
